f959c91d28e5286ae6836b27c877c084639f8209
Louis M <90392650+Lhans22@users.noreply.github.com>
2024-03-26 16:31:41 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-//l'id doit etre égal a l'id de la bare de navigation et il ne doit pas avoir deux fois le même id
-//image est optionelle
+//il ne doit pas avoir plusieurs fois le même id
+//l'image est optionelle
 const element =[
 		{
 			id : "first",
+			navTitre:"premier page",
 			titre:"Ceci est un titre",
 			description:"je suis une para graphe hhhhhhhhhhhhhsdfhjfghn,kfjhdfsnbbfkgbkfjvbkbkjbkxdfbjkfcbjkv bjkbjkbjkbjkxbjfbjkbjkxxfbjkvbjkvfbj kvbjjkbvfkfbjbjvkxbjkvbvbjkbjkfvbjkvbjkxbjkbjkxbjkvbjkbjk bjvfbjkxbjkbjkxbbjkbjkxfxbjkbjkvbjkvfbjkxb jkxvbbjkbjbjfxbjvfvfxbjkbjkxfbjbjbjkbjkbjkvfbjkvbjkxvbvbjbjkxfbjkvbjkvfbvfbjkvxbjkbjkfbjbjvfbjkvfbjkxvbjkvbjkvbjkbjk",
 			image : "panda.jpg",
 		},
 		{
 			id : "test",
-			titre:"",
+			navTitre:"deuxième page",
+			titre:"test test ",
+			description:"dsfshdfskjdfkjfsdf",
+		},
+		{
+			id : "test2",
+			navTitre:"troisième page",
+			titre:"test2",
 			description:"",
 			image : "",
 		},
 		{
-			id : "",
-			titre:"",
-			description:"",
-			image : "",
-		},
-		{
-			id : "",
+			id : "test3",
+			navTitre:"quatrième page",
 			titre:"",
 			description:"",
 			image : "",
@@ -31,8 +34,20 @@ const element =[
 /*----------------------------------NE PAS TOUCHER EN DESSOU DE CETTE LIGNE------------------------------------------*/
 
 ElementsHtml="";
+navHtml="";
 
 element.forEach(function (item) {
+
+	// la barre de navigation
+	navHtml=navHtml+"<a class='item' href='#"+item.id+"'>"+item.navTitre+"</a>";
+
+});
+
+invertElement = element.reverse();
+
+invertElement.forEach(function (item) {
+
+	//le contenu des pages
 	ElementsHtml=ElementsHtml+"<section id='"+item.id+"' class=''>"
 		ElementsHtml=ElementsHtml+"<h1>"+item.titre+"</h1>"
 		ElementsHtml=ElementsHtml+"<div class='ui segment'>"+item.description+"</div>"
@@ -44,6 +59,12 @@ element.forEach(function (item) {
 	ElementsHtml=ElementsHtml+"</section>"
 });
 
+
 $(document).ready(function(){
     $(".main").html(ElementsHtml);
+    $(".navigationMenu").html(navHtml);
+    $("a").on( "click", function( event ) {
+    	$("a").removeClass("active")
+    	$( this ).addClass("active")
+    });
 });
